f488a4c46a26731d0594e3a454d72a8d145cfc80
Laurie Gloge <laurie@friendlyroad.com>
2018-08-27 12:02:41 -0600

    added like method with console.log

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -8,6 +9,10 @@ class Movies extends Component {
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    console.log("Like Clicked", movie);
   };
 
   render() {
@@ -24,6 +29,7 @@ class Movies extends Component {
             <th>Stock</th>
             <th>Rate</th>
             <th />
+            <th />
           </thead>
           <tbody>
             {this.state.movies.map(movie => (
@@ -32,6 +38,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
