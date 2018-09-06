aab78cab0af202e12b1cc6bac670d6378c05ef00
Laurie Gloge <laurie@friendlyroad.com>
2018-09-06 15:30:33 -0600

    error message the long way

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -29,11 +29,26 @@ class LoginForm extends Component {
 
     console.log("Submitted");
   };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() == "") return "Username is required.";
+      // more rules
+    }
+    if (name === "password") {
+      if (value.trim() == "") return "Password is required.";
+      // more rules
+    }
+  };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errrors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
