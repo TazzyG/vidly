fd4669ed183dcab2b68da4b1170b99ccf7aff139
Laurie Gloge <laurie@friendlyroad.com>
2018-09-06 15:57:07 -0600

    refactored validation with object destruction and options const

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -18,12 +18,11 @@ class LoginForm extends Component {
       .label("Password")
   };
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false
-    });
-    if (!result.error) return null;
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
     const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
+    for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
   };
   //   componentDidMount() {
