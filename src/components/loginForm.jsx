586194fd4f511f9eb84e61dc08b1eac3286f15df
Laurie Gloge <laurie@friendlyroad.com>
2018-09-06 15:17:22 -0600

    display error messages

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -23,8 +23,7 @@ class LoginForm extends Component {
   handleSubmit = e => {
     e.preventDefault();
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
     // therefore we won't call the server
 
@@ -37,7 +36,7 @@ class LoginForm extends Component {
     this.setState({ account });
   };
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -47,12 +46,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
