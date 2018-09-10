cce74e7ef18ec32b1630022bb5c6465a087a4d6e
Laurie Gloge <laurie@friendlyroad.com>
2018-09-10 15:35:18 -0600

    created a register form

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {
+      // username: "Username is required."
+      // password: "Password must be more than 5 characters"
+    }
+  };
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .min(5)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
+  };
+
+  doSubmit = () => {
+    // call the server
+    console.log("Submitted as Registered");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
